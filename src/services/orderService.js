6bd191f1b60b795d02d09cf406353cc5e496bf52
Rooney Lee <kroonstazy@gmail.com>
2023-10-05 12:23:45 +0900

    chore: 사용하지 않는 코드 삭제

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -10,33 +10,24 @@ const {
   cancelOrdersDao,
 } = orderDao;
 
-//주문생성 (카트에서 -> 오더로)
+// 주문 생성 (카트에서 -> 오더로)
 const createOrderService = async (userId, totalPrice) => {
   const customerCart = await customerCartDao(userId);
   if (customerCart.length == 0) throwError(400, 'NO_ORDERS')
   return MoveCartToOrderDao(userId, totalPrice);
 };
 
-//주문 목록 불러오기
+// 주문 목록 불러오기
 const getOrderListService = async (userId) => {
   return await getOrderListDao(userId);
 };
 
-//주문 상세 불러오기
+// 주문 상세 불러오기
 const getOrderDetailService = async (userId, orderId) => {
   return await getOrderDetailDao(userId, orderId);
 };
 
-// //주문하기 
-// const placeOrderService = async (userId, totalPrice) => {
-//   const customers_Credit = await checkCreditDao(userId);
-//   const customerCart = await customerCartDao(userId);
-//   if ((customers_Credit < totalPrice) || (totalPrice < 0)) throwError(400, 'NOT_ENOUGH_CREDIT')
-//   if (customerCart.length == 0) throwError(400, 'NO_ORDERS')
-//   return MoveCartToOrderDao(userId, totalPrice);
-// };
-
-//주문취소하기
+// 주문취소 - 구현 못함, 역량 부족, 시간 부족
 const cancelOrdersService = async (userId, orderId, totalPrice) => {
   const orderStatus = await checkOrderStatusDao(orderId);
   if (orderStatus == orderStatusEnum.CANCELED) throwError(400, 'ALREADY_CANCELED')
